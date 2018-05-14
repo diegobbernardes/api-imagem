9d88eac4311835e36255e61a0f9e50ff93bfcc19
DESKTOP-N4VS9FC\Diego <diegob.bernardes@gmail.com>
2018-05-14 20:49:07 -0300

    Adicionado arquivo teste da api

--- teste.php
+++ teste.php
@@ -1,11 +1,53 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-require_once('GoogleTranslate.class.php');
-$source = 'pt-br';
-$target = 'en';
-$text = "testando tradução batata";
-$translation = GoogleTranslate::translate($source, $target, $text);
-echo '<pre>';
-print_r($translation);
-echo '</pre>';
+function CallAPI($method, $url, $data = false)
+{
+    $curl = curl_init();
+
+    switch ($method)
+    {
+        case "POST":
+            curl_setopt($curl, CURLOPT_POST, 1);
+
+
+
+            if ($data)
+                curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+            break;
+
+        case "PUT":
+            curl_setopt($curl, CURLOPT_PUT, 1);
+            break;
+        default:
+            if ($data)
+                $url = sprintf("%s?%s", $url, http_build_query($data));
+    }
+
+    // Optional Authentication:
+    curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+
+    curl_setopt($curl, CURLOPT_URL, $url);
+
+    $result = curl_exec($curl);
+
+    curl_close($curl);
+
+    return $result;
+}
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $tmpfile = $_FILES['image']['tmp_name'];
+    $filename = basename($_FILES['image']['name']);
+    $data = array(
+        'uploaded_file' => curl_file_create($tmpfile, $_FILES['image']['type'], $filename)
+    );
+
+    echo  CallAPI("POST","visionLabel.php", $data);
+
+}
+?>
+<body>
+	<form method="post" action="teste.php" enctype="multipart/form-data">
+        <input type="file" name="image" />
+		<input type="submit" />
+	</form>
+</body>
